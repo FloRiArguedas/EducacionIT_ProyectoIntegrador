495cd2d8b8f7eab9f4e4c0516c05d3f56bb0b709
FloRiArguedas <floricreativa@gmail.com>
2024-09-08 00:02:09 -0600

    Se agregan cards y UseTitulo

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,20 @@
+import './Card.scss'
+
 const Card = () => {
   return (
-    <div>Card</div>
+    <div className="card">
+      <article className="card__article">
+        <div className="card__image-container">
+          <img src="{{imagen}}" alt="{{nombre}}" className="card__image" />
+        </div>
+        <div className="card__content">
+          <h2 className="card__heading"> "{'{nombre}'}"  </h2>
+          <div className="card__description">
+            <p>"{'{descripcion}'}"</p>
+          </div>
+        </div>
+      </article>
+    </div> 
   )
 }
 
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,3 +1,4 @@
+import Card from "../components/Card";
 import "./Inicio.scss";
 
 const Inicio = () => {
@@ -18,21 +19,9 @@ const Inicio = () => {
         </header>
 
         <div className="cards-container" id="cardsContainer">
-          {/* {{! CARDS PARA LOS PRODUCTOS }} */}
 
-          {/* <div className="card">
-            <article className="card__article">
-              <div className="card__image-container">
-                <img src="{{imagen}}" alt="{{nombre}}" className="card__image" />
-              </div>
-              <div className="card__content">
-                <h2 className="card__heading"> {{ "nombre" }} </h2>
-                <div className="card__description">
-                  <p>{{ "descripcion" }}</p>
-                </div>
-              </div>
-            </article>
-          </div> */}
+          <Card />
+        
         </div>
       </section>
     </main>
