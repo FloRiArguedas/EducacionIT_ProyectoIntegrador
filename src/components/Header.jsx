2e52d7b2764da2a18a32e09403635665cb093451
FloRiArguedas <floricreativa@gmail.com>
2024-09-08 00:21:06 -0600

    Se agrega navbar con menuItems

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,11 @@
 import './Header.scss'
+import Navbar from './Navbar'
 const Header = () => {
   return (
     <header className="main-header">
     <input type="checkbox" id="menu" />
 
-    <nav className="nav-bar">
-      <ul className="nav-bar__nav-list">
-        <li className="nav-bar__nav-item">
-          <a href="index.html" className="nav-bar__nav-link">
-            Inicio
-          </a>
-        </li>
-        <li className="nav-bar__nav-item">
-          <a href="#" className="nav-bar__nav-link">
-            Alta
-          </a>
-        </li>
-        <li className="nav-bar__nav-item">
-          <a href="views/nosotros.html" className="nav-bar__nav-link">
-            Nosotros
-          </a>
-        </li>
-        <li className="nav-bar__nav-item">
-          <a href="views/contacto.html" className="nav-bar__nav-link">
-            Contacto
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <Navbar />
 
     <div className="search-bar">
       <div className="search-bar__logo-container">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,18 @@
+import menuItems from "../constants/menuItems";
+
 const Navbar = () => {
   return (
-    <h1>NavBar</h1>
+    <nav className="nav-bar">
+      <ul className="nav-bar__nav-list">
+        {menuItems.map((item) => (
+          <li className="nav-bar__nav-item" key={item.id}>
+            <a href={item.ruta} className="nav-bar__nav-link">
+              {item.nombre}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
