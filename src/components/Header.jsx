11f798bdc507a524762ff26db38f2dbeb069e5b1
FloRiArguedas <floricreativa@gmail.com>
2024-09-08 00:27:40 -0600

    Se agrega SearchBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,16 @@
 import './Header.scss'
 import Navbar from './Navbar'
+import SearchBar from './SearchBar'
 const Header = () => {
   return (
     <header className="main-header">
-    <input type="checkbox" id="menu" />
+      <input type="checkbox" id="menu" />
 
-    <Navbar />
+      <Navbar />
 
-    <div className="search-bar">
-      <div className="search-bar__logo-container">
-        <img
-          className="search-bar__logo3d4u"
-          src="/img/Logo3D4UOriginal.webp"
-          alt="Logo 3D4U Orginal Morado con Amarillo"
-        />
-      </div>
-      <form action="#" className="search-bar__form-container">
-        <label htmlFor="busqueda" className="search-bar__form-label">
-          Buscar Figuras
-        </label>
-        <input type="search" className="search-bar__form-search" id="busqueda" />
-        <input type="submit" className="search-bar__form-submit" value="Buscar" />
-      </form>
-      <div className="search-bar__carrito-container">
-        <i className="fa-solid fa-cart-shopping"></i>
-      </div>
-      <div className="menu-toogle">
-        <label htmlFor="menu" className="menu-toogle__label">
-          <span className="menu-toogle__top-bread"></span>
-          <span className="menu-toogle__meat"></span>
-          <span className="menu-toogle__bottom-bread"></span>
-        </label>
-      </div>
-    </div>
-  </header>
+      <SearchBar />
+
+   </header>
   )
 }
 
