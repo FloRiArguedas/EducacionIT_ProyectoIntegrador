ede7d8b51662574ad093dac98bda940086966270
FloRiArguedas <floricreativa@gmail.com>
2024-09-08 00:44:45 -0600

    Se finaliza config de NavItem

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import menuItems from "../constants/menuItems";
+import NavItem from "./NavItem";
 
 const Navbar = () => {
   return (
     <nav className="nav-bar">
       <ul className="nav-bar__nav-list">
-        {menuItems.map((item) => (
-          <li className="nav-bar__nav-item" key={item.id}>
-            <a href={item.ruta} className="nav-bar__nav-link">
-              {item.nombre}
-            </a>
-          </li>
-        ))}
+
+        {
+        menuItems.map(item => (
+          <NavItem item={item} key={item.id} />
+        ))
+        }
+
       </ul>
     </nav>
   );
