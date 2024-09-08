849485330216f6b74890edf7913cd5e774ed2ae7
FloRiArguedas <floricreativa@gmail.com>
2024-09-08 01:08:50 -0600

    Se avanza con helpers y constants

--- src/helpers/helper.Peticiones.HTTP.js
+++ src/helpers/helper.Peticiones.HTTP.js
@@ -5,13 +5,9 @@ export const helperPeticionesHttp = async (url, options) => {
         if(!res.ok) throw new Error ('No fue posible realizar', res.status)
         const data = await res.json()
         return data
+
     } catch (error) {
         console.error('HelperPeticionesHttp')
     }
-
-
-
-
-
 
 }
