09d62f4ac89b7f2e5bcee260d6d02cc5e6c249f2
FloRiArguedas <floricreativa@gmail.com>
2024-10-06 23:46:25 -0600

    Se añaden detalles a la página de contacto

--- 01.Front/bc-71370-Integrador-etapa3/src/pages/Contacto.jsx
+++ 01.Front/bc-71370-Integrador-etapa3/src/pages/Contacto.jsx
@@ -3,12 +3,128 @@ import './Contacto.scss'
 
 const Contacto = () => {
 
-  useTitulo('Contacto')
+  useTitulo('Contáctenos')
   
   return (
    <>
-    <h1>Contacto</h1>
-    <hr />
+    <div className="Banner-Container-fixed">
+      <img className="Banner-Container-fixed__ContactImage" src="/img/mano3D.webp" alt="Mano de humano, tocando la mano en 3D del modelo"/>
+    </div>
+
+    <div className="head">
+      <h1>COMUNICATE CON NOSOTROS</h1>
+      <p>
+        ¿Tienes una figura en mente? 
+      </p>
+      <p>Cuéntanos los detalles y haremos tu deseo
+        realidad.</p>
+    </div>
+
+    {/* <!-- FORMULARIO DE CONTACTO --> */}
+    <div className="Form-Container">
+      <div className="Form-Container__Form-contact">
+        <form>
+          <div className="Form-Container__Form-contact__grupo">
+            <label for="label-nombre">Nombre: * </label>
+            <input
+              type="text"
+              name="nombre"
+              id="label-nombre"
+              placeholder="Eje: Floricela"
+              required
+            />
+          </div>
+
+          <div className="Form-Container__Form-contact__grupo">
+            <label for="label-apellido">Apellido: </label>
+            <input
+              type="text"
+              name="apellido"
+              id="label-apellido"
+              placeholder="Eje: Arguedas"
+            />
+          </div>
+
+          <div className="Form-Container__Form-contact__grupo">
+            <label for="label-email">Correo Electrónico: * </label>
+            <input
+              type="email"
+              name="email"
+              id="label-email"
+              placeholder="Eje: 3d4u.servicioalcliente@gmail.com"
+              required
+            />
+          </div>
+
+          <div className="Form-Container__Form-contact__grupo">
+            <label for="label-numero">Número de teléfono: </label>
+            <input type="tel" name="numero" id="label-numero" />
+          </div>
+
+          <div className="Form-Container__Form-contact__grupo">
+            <div id="description">
+              <label for="label-descripcion">Cuéntanos los detalles: * </label>
+              <textarea
+                name="detalles"
+                id="label-descripcion"
+                cols="20"
+                rows="10"
+                placeholder="Describa aquí lo que desea en su pedido"
+              ></textarea>
+            </div>
+          </div>
+
+          <div className="Form-Container__Form-contact__grupo">
+            <div className="campo_requerido_container">
+              <p id="requiered">* Campos requeridos</p>
+            </div>
+            <div className="button">
+              <button id="boton_form">Enviar Solicitud</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+
+    {/* <!-- Medios de Comunicacion --> */}
+
+    <div class="bloque-MediosyRedes">
+      <div>
+        <h2>Contáctanos en nuestros medios de comunicación</h2>
+        <div class="bloque-MediosyRedes__MediosComunicacion">
+          <div class="bloque-MediosyRedes__grupo-canal">
+            <p class="whatsapp">
+              <i class="fa-brands fa-whatsapp"></i> Escríbenos a nuestro <a href="https://wa.me/50670128588" target="_blank"><i> WhatsApp</i></a>
+            </p>
+          </div>
+
+          <div class="bloque-MediosyRedes__grupo-canal">
+            <p class="email">
+              <i class="fa-solid fa-envelope"></i> Escríbenos a nuestro <a href="mailto:3d4u.servicioalcliente@gmail.com" target="_blank"><i> Correo Electrónico</i></a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h2>Siguenos en nuestras Redes Sociales</h2>
+
+        <div class="bloque-MediosyRedes__RedesSociales">
+          <div class="bloque-MediosyRedes__grupo-canal">
+            <p class="instagram">
+              <i class="fa-brands fa-instagram"></i> Instagram: <a href="https://www.instagram.com/3d4u.cr/?igsh=OWdnOWltNno5bDR3" target="_blank"> @3D4U.cr</a>
+            </p>
+          </div>
+
+          <div class="bloque-MediosyRedes__grupo-canal">
+            <p class="tiktok">
+              <i class="fa-brands fa-tiktok"></i> Tik Tok:<a href="https://www.tiktok.com/@3d4u.cr" target="_blank"> @3D4U.cr</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
    </>
   )
 }
